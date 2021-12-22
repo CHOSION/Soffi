eca48eafbf8873b4f493b2efe900a9140d803f13
CHOSION <chosion0107@gmail.com>
2021-12-22 12:01:00 +0900

    211222

--- Soffi/ViewController.swift
+++ Soffi/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var quoteLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapQuoteGeneratorButton(_ sender: Any) {
+    }
+    
 }
 
