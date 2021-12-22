e6ae0f395f6c5b3510386b4b45a25f0e332a4666
CHOSION <chosion0107@gmail.com>
2021-12-22 11:10:46 +0900

    Initial Commit

---
+++ Soffi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soffi
+//
+//  Created by 조세연 on 2021/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
